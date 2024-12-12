d44e4c32dc4f8e50aa5021fff80abe926f977fbe
muzijerrymuller <jerrymuller883@gmail.com>
2024-12-11 18:42:13 -0800

    app controller

---
+++ controllers/AppController.js
@@ -0,0 +1,28 @@
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
+
+class AppController {
+  /**
+   * return if Redis is alive & DB is alive
+   */
+  static getStatus(request, response) {
+    const status = {
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
+    };
+    response.status(200).send(status);
+  }
+
+  /**
+   * return the amount of users
+   */
+  static async getStats(request, response) {
+    const stats = {
+      users: await dbClient.nbUsers(),
+      files: await dbClient.nbFiles(),
+    };
+    response.status(200).send(stats);
+  }
+}
+
+export default AppController;
